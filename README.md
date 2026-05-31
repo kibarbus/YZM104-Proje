ed4b667e0ad1e36906649271b755e6c05e697c1a
Busra Kibar <kibarbus@gmail.com>
2026-05-31 17:56:08 +0300

    assets klasoru ve openal32.dll dosyasinin otomatik kopyalanmasi icin CMake guncellendi.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # YZM104-Proje
 Oyun Projesi: Tetris
+
+![C++ 17](https://img.shields.io/badge/language-C++17-D6A2E8)
+![SFML 2.6.1](https://img.shields.io/badge/libraries-SFML--2.6.1-B19FFB)
+![CMake](https://img.shields.io/badge/build-CMake-C3B1E1)
+
---
+++ build/CMakeFiles/tetris.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/DellX/Desktop/programlama 2 proje/YZM104-Proje/src/BilgiAlani.cpp" "CMakeFiles/tetris.dir/src/BilgiAlani.cpp.obj" "gcc" "CMakeFiles/tetris.dir/src/BilgiAlani.cpp.obj.d"
+  "C:/Users/DellX/Desktop/programlama 2 proje/YZM104-Proje/src/OyunAlani.cpp" "CMakeFiles/tetris.dir/src/OyunAlani.cpp.obj" "gcc" "CMakeFiles/tetris.dir/src/OyunAlani.cpp.obj.d"
+  "C:/Users/DellX/Desktop/programlama 2 proje/YZM104-Proje/src/TetrisBloklari.cpp" "CMakeFiles/tetris.dir/src/TetrisBloklari.cpp.obj" "gcc" "CMakeFiles/tetris.dir/src/TetrisBloklari.cpp.obj.d"
+  "C:/Users/DellX/Desktop/programlama 2 proje/YZM104-Proje/src/main.cpp" "CMakeFiles/tetris.dir/src/main.cpp.obj" "gcc" "CMakeFiles/tetris.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
